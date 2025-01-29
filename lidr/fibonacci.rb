6da6bc886407535663515fddf6b5606d1d9ddedb
Inga Maholwana <imaholwana023@student.wethinkcode.co.za>
2025-01-29 21:11:42 +0200

    am keeping track of the recusiveness now let me work on thee merge and sorting array-

--- lidr/fibonacci.rb
+++ lidr/fibonacci.rb
@@ -1,12 +1,25 @@
 #first method will use iteration to take a given number and create the fibo sequence of that number long
 def iterfibs(n)
-  fibo = [0, 1]
-  i = 2
-  while i < n
-    fibo.push(fibo[i - 1] + fibo[i - 2]) 
+  fibo = [0, 1] # start the first index 0 and the 2nd 1  
+  i = 2 # start workin from the second index beceuse already set on the first
+  while i < n #run as long less than n
+    fibo.push(fibo[i - 1] + fibo[i - 2]) #adds the last (the newest result) number and the second last (to the previos result before it) together
     i += 1
   end
   fibo
 end
 
-p iterfibs(9)
+# p iterfibs(9)
+# now it will be recursive
+def fibs_rec(n)
+  if n == 1
+    return [0] #because its starts at 0
+  elsif n == 2
+    return [0, 1] #because the first two Fibonacci numbers are 0 and 1
+  else
+    fibo = fibs_rec(n - 1) #keeps reducing until it reaches 1 of the base cases 
+    fibo.push(fibo[-1] + fibo[-2])#last element in array second last element then pushed in array
+  end
+end
+
+# p fibs_rec(8)
---
+++ lidr/mergeAlgo.rb
@@ -0,0 +1,22 @@
+#this is going to be the merge sort which divides an array into half until its at their
+# once at their smallest parts combines them in order of assending or decending order
+# how do i half and array in ruby(generally) then how do i sort it to be in ascending order
+# how do then recombine the array
+# algo for merg sort(firstele,lastelem)
+# if (firstele < lastelem)
+# middle (firstele + lastelem / 2 )
+# algo for merge sort (middle+1 ,lastelem)
+# combine (firstele, middle, lastelem)
+
+def merge_sort(array)
+  puts "This was printed recursively"  # To track recursive calls
+
+  return array if array.length <= 1  # Base case: single-element arrays are already sorted
+
+  mid = array.length / 2 
+  left_half = merge_sort(array[0...mid]) 
+  right_half = merge_sort(array[mid..-1])  
+  
+end
+
+p merge_sort([3, 2, 1, 13, 8, 5, 0, 1])
